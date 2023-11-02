7b8c4ec34bf2351b655c829ed88f71f99e5004b3
Eyad Kobatte <eyad.kobatte@meltwater.com>
2023-11-02 17:48:12 +0000

    working grep

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = slowgrep::Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    if let Err(error) = slowgrep::run(config) {
+        println!("Error running application: {error}");
+        process::exit(1)
+    };
+}
