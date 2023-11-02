2266e22c8e916ea88633b59a5f4db1118483e2c2
Eyad Kobatte <eyad.kobatte@meltwater.com>
2023-11-02 17:50:18 +0000

    move to std error stream

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = slowgrep::Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     if let Err(error) = slowgrep::run(config) {
-        println!("Error running application: {error}");
+        eprintln!("Error running application: {error}");
         process::exit(1)
     };
 }
